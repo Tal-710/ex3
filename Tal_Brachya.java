8355758ca3723dca32eb53a3a088655921fe28c1
talb75575 <talbrachya10@gmail.com>
2024-01-26 11:17:59 +0200

    first commit

---
+++ Tal_Brachya.java
@@ -0,0 +1,9 @@
+public class Tal_Brachya{
+    public static void main(String []args)
+    {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("Tal_Brachya"+i);
+        }
+    }
+}
